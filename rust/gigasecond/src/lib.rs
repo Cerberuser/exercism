39a0d82eb418b2b48d7d6f7134aa0a2253d3f8b9
k.bagrov <k.bagrov@g.nsu.ru>
2019-05-29 23:32:39 +0700

    Modified gigasecond example

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,10 +1,24 @@
 use chrono::{DateTime, Utc};
+use num_format::{Locale, ToFormattedString};
 
-// Returns a Utc DateTime one billion seconds after start.
+const DURATION_IN_SECONDS: i64 = 1_000_000_000;
+
+/// Returns a Utc DateTime one billion seconds after start.
+/// 
+/// ## Panics
+/// This function will panic on overflow.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-	start
-		.checked_add_signed(chrono::Duration::seconds(1_000_000_000))
-		.unwrap_or_else(|| {
-			panic!("Overflow while adding duration: source DateTime was {}, so one billion seconds later is too late", start)
-		})
+    start
+        .checked_add_signed(chrono::Duration::seconds(DURATION_IN_SECONDS))
+        .unwrap_or_else(|| {
+            panic!(
+                "
+			Overflow while adding duration: 
+			source DateTime was {}, 
+			so {} seconds later is too late
+",
+                start,
+                DURATION_IN_SECONDS.to_formatted_string(&Locale::en)
+            )
+        })
 }
