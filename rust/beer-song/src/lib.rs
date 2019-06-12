8b350711de877f1b26585573ea66b27cd53ab2e2
k.bagrov <k.bagrov@g.nsu.ru>
2019-06-12 18:29:48 +0700

    First iteration of Beer song in Rust; Export command for Pharo

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,17 @@
+use itertools::Itertools;
+
 pub fn verse(n: i32) -> String {
-    unimplemented!("emit verse {}", n)
+    if n == 0 {
+        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".into()
+    } else if n == 1 {
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".into()
+    } else if n == 2 {
+        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".into()
+    } else {
+        format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1)
+    }
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..=start).rev().map(verse).join("\n")
 }
