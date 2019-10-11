cb27db9472948bbb286c2d58cff17c1fa97326fc
k.bagrov <k.bagrov@g.nsu.ru>
2019-10-11 22:51:52 +0700

    Rust: updated Bob exercise

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -7,53 +7,26 @@
 //! Of course, if you feel like you want to reimplement all this yourself -
 //! we won't stop you in any way. Just remember not to harm Bob.
 
-/// Public type definition for private struct.
-///
-/// It is held here to be used as guard for [`Message`][Message] enum variants. Since the only field of this structure
-/// is private, it is forbidden to construct `Message` from outside this crate.
-///
-/// For more information, see the documentation on enum itself.
-///
-/// [Message]: self::Message
-pub struct Internal(());
-
 /// Represents the categories of possible Bob's answers.
 ///
 /// Since there is a finite subset of what Bob can say, and it doesn't have any parameters
 /// based on the input (we simply choose one way or another, but don't add, for example,
 /// any arbitrary number), we can use an enum without any values - the replies are `&'static str`
 /// values hard-coded into enum implementation.
-///
-/// Although, we add some private fields to any variant simply to assure that it won't be
-/// constructed in unpredictable way (and to make ourselves free to change this implementation
-/// on possible changes of requirements without making a breaking change).
 pub enum Message {
     /// Message without any content.
-    Silence(Internal),
+    Silence,
     /// Any question asked quietly.
-    Question(Internal),
+    Question,
     /// Any shouts, excluding questions.
-    Shout(Internal),
+    Shout,
     /// Any question being yelled.
-    ShoutQuestion(Internal),
+    ShoutQuestion,
     /// Default value for all unhandled causes.
-    Whatever(Internal),
+    Whatever,
 }
 
 impl Message {
-    /// Converts the message string into corresponding enum variant.
-    ///
-    /// This is one of two ways to construct the `Message` instance from outside this crate.
-    /// We must make sure that you won't force Bob to say something if he wasn't asked.
-    pub fn incoming(message: &str) -> Self {
-        // First, check if the message is nothing. This is the special case,
-        // since this check is independent from the others.
-        Message::try_as_silence(message)
-            // If this returns None, i.e. message is not nothing, we must classify it.
-            // We do it simply by calling the dedicated function.
-            .unwrap_or_else(|| Self::classify(message))
-    }
-
     /// Consumes enum, handing out the corresponding Message text.
     ///
     /// Of course, you can try and match the `Message` in your own code. We don't take any warranty for what you get,
@@ -63,44 +36,27 @@ impl Message {
     pub fn reply(self) -> &'static str {
         use Message::*;
         match self {
-            Question(_) => "Sure.",
-            Shout(_) => "Whoa, chill out!",
-            ShoutQuestion(_) => "Calm down, I know what I'm doing!",
-            Silence(_) => "Fine. Be that way!",
-            Whatever(_) => "Whatever.",
+            Question => "Sure.",
+            Shout => "Whoa, chill out!",
+            ShoutQuestion => "Calm down, I know what I'm doing!",
+            Silence => "Fine. Be that way!",
+            Whatever => "Whatever.",
         }
     }
 
-    // Attempts to instantly classify message as silence.
-    //
-    // It returns Some(Message.Silence), if the incoming message consist only of whitespace,
-    // and None otherwise.
-    fn try_as_silence(message: &str) -> Option<Message> {
-        if message.trim().len() == 0 {
-            Some(Message::Silence(Internal(())))
-        } else {
-            None
-        }
-    }
-
-    /// Function to classify non-empty incoming messages.
+    /// Classifies incoming message strings.
     /// 
-    /// It is used internally by the [incoming] function, but can also be called independently.
-    /// 
-    /// ## Panics
-    /// This function will panic in debug mode if the message is of "silence" class (i.e. contain only whitespace).
-    /// In release mode this message will be erroneously classified as [Whatever], be careful!
-    /// 
-    /// [incoming]: Message::incoming
-    /// [Whatever]: Message::Whatever
+    /// We strongly encourage you to use this function and not to construct `Message` yourself.
     pub fn classify(message: &str) -> Message {
-        debug_assert!(!message.trim().is_empty());
-        let internal = Internal(());
+        if message.trim().is_empty() {
+            Message::Silence
+                    } else {
         match (Self::is_shout(message), Self::is_question(message)) {
-            (true, true) => Message::ShoutQuestion(internal),
-            (true, false) => Message::Shout(internal),
-            (false, true) => Message::Question(internal),
-            (false, false) => Message::Whatever(internal),
+            (true, true) => Message::ShoutQuestion,
+            (true, false) => Message::Shout,
+            (false, true) => Message::Question,
+            (false, false) => Message::Whatever,
+        }
         }
     }
 
@@ -118,7 +74,7 @@ impl Message {
     // This function returns true if the incoming message is a question,
     // i.e. it ends with the question mark.
     fn is_question(message: &str) -> bool {
-        message.trim().chars().last() == Some('?')
+        message.trim().ends_with('?')
     }
 }
 
@@ -130,5 +86,5 @@ impl Message {
 /// 
 /// [Message]: self::Message
 pub fn reply(message: &str) -> &str {
-    Message::incoming(message).reply()
+    Message::classify(message).reply()
 }
